4c1f336bad5cdcf71b1e62d29af0db468b35e78d
Stefan Pijnacker <stefanpijnacker@gmail.com>
2021-06-12 11:11:18 +0200

    GameVars and tool added

---
+++ Assets/Scripts/GameVariables.cs
@@ -0,0 +1,15 @@
+// Dit kunnen we gebruiken om game stats op 1 plek te tweaken.
+// Bijv. wel hebben 3 enemies dan hoeven niet in 3 classes shit te bewerken want we kunnen dan
+// dit gewoon aanpassen :)
+public static class GameVariables
+{
+    // Player Health
+    public const float START_MAX_HEALTH = 100f;
+
+    // Player Weapons
+    public const float SWORD_DAMAGE = 5f;
+    public const float PROJECTILE_DAMAGE = 10f;
+
+    // Enemy Damage
+    public const float ENEMY_DAMAGE = 5f;
+}
