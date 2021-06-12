9b9721ab9e727cd010ce920d7a55c795c3269a52
KingDwarf <kylevdboom@hotmail.nl>
2021-06-12 20:32:20 +0200

    Changed the level switch system. It now uses scene

--- Assets/Scripts/Managers/LevelSwitchManager.cs
+++ Assets/Scripts/Managers/LevelSwitchManager.cs
@@ -1,21 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSwitchManager : MonoBehaviour
 {
     public static LevelSwitchManager Instance;
-
-    [SerializeField] List<GameObject> m_levels;
-    
-    GameObject m_currentlevel;
-
 
     void Awake()
     {
         if(Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -23,28 +20,13 @@ public class LevelSwitchManager : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        for (int l = 0; l < m_levels.Count; l++)
-        {
-            m_levels[l].SetActive(false);
-        }
 
-        m_levels[0].SetActive(true);
-        m_currentlevel = m_levels[0];
-    }
-
+    /// <summary>
+    /// Load de megegeven scene
+    /// </summary>
+    /// <param name="_levelname">Het level dat je wilt laden. Zorg dat deze in de build setting zit!</param>
     public void LoadLevel(string _levelname)
     {
-        m_currentlevel.SetActive(false);
-
-        for (int l = 0; l < m_levels.Count; l++)
-        {
-            if(m_levels[l].name == _levelname)
-            {
-                m_levels[l].SetActive(true);
-                m_currentlevel = m_levels[l];
-            }
-        }
+        SceneManager.LoadScene(_levelname);
     }
 }
