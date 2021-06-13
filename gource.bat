eb83846f3715098c77c35a2a2fbd64ef134ce7a7
Stefan Pijnacker <stefanpijnacker@gmail.com>
2021-06-13 20:36:41 +0200

    Gource setup

---
+++ gource.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Gource\gource.exe" --highlight-users -1080x720 -e 0.01 --font-size 24 --camera-mode track --multi-sampling --seconds-per-day 45 --title "GMTK 2021 Jam - Souls Reflection" --background 222222 --user-image-dir avatars/ --hide filenames --hash-seed GMTK2021 --bloom-intensity 0.4 --auto-skip-seconds 0.01 --hide filenames --disable-auto-rotate --multi-sampling 
+pause 
